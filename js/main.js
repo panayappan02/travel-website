68f3f3e12be255d87323d6ea6fb6e7d7e334434c
panayappan01 <panayappanlakshmanan09@gmail.com>
2022-08-14 00:02:18 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// Global variables
+const navMobile = document.getElementById("nav");
+const menuToggleIcon = document.getElementById("menu-toggle-icon");
+const headerElement = document.getElementById("header");
+const navLinks = document.querySelectorAll(".nav-mobile .list-link");
+// const menuToggleIcon = document.getElementById("menu-toggle-icon");
+
+console.log(navLinks);
+
+// Toggle navigation
+const toggleMenu = () => {
+  navMobile.classList.toggle("active");
+  headerElement.classList.toggle("active");
+};
+
+menuToggleIcon.addEventListener("click", toggleMenu);
+//Add/remove header border bottom on scroll
+const headerScrollEvent = () => {
+  if (this.scrollY >= 30) {
+    headerElement.classList.add("active-scroll");
+  } else {
+    headerElement.classList.remove("active-scroll");
+  }
+};
+
+window.addEventListener("scroll", headerScrollEvent);
+
+// Add selected link styles to the link clicked and close navigation when clicking the links
+navLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    navMobile.classList.remove("active");
+    headerElement.classList.remove("active");
+
+    let current = document.getElementsByClassName("current");
+    current[0].className = current[0].className.replace(" current", "");
+    link.className += " current";
+  })
+);
+
+// Scroll Reveal
+const sr = ScrollReveal({
+  distance: "25px",
+  duration: 2500,
+});
+
+sr.reveal(`.image-left, .popular-destinations-data, .plan-trip-data`, {
+  origin: "left",
+});
+
+sr.reveal(".image-center", {
+  origin: "bottom",
+});
+
+sr.reveal(`.image-right, .showcase-data`, {
+  origin: "right",
+});
+
+sr.reveal(".beach-data-wrapper", {
+  origin: "bottom",
+  interval: 250,
+});
+
+sr.reveal(".newsletter-container", {
+  origin: "top",
+});
+
+sr.reveal(`.footer-container-inner > div, .footer-separator, .copyright`, {
+  origin: "bottom",
+  interval: 250,
+});
